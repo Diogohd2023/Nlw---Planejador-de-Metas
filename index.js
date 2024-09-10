418791430d0836b329c65cbdd8c5e86852869afc
Diogo <diogodias134@gmail.com>
2024-09-10 12:31:00 -0300

    fundamentos 003

---
+++ index.js
@@ -0,0 +1,20 @@
+
+let meta = {
+    value: "ler um livro por mes",
+    checked: false,  
+    }
+
+let metas = [
+    meta,
+    {
+        value: "caminhar 20 minutos todos os dias",
+        checked: true
+    }
+]
+
+console.log(metas[1].value)
+
+// Function  // Arrow function
+//const criarMeta = () => {}
+
+//function criarMeta() {}
