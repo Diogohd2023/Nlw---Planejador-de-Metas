3226d46153e13f03974d5a3c16222df28ec467bc
Diogo <diogodias134@gmail.com>
2024-09-10 18:02:50 -0300

    listar metas

--- index.js
+++ index.js
@@ -21,6 +21,32 @@ const cadastrarMeta = async () => {
     )
 }
 
+const listarMetas = async () => {
+    const respostas = await checkbox({
+        message: "Use as setas para mudar de meta, o espaço para marcar ou desmarcar e o Enter para finalizar essa etapa",
+        choices: [...metas]
+    })
+
+    if (respostas.length == 0){
+        console.log("Nenhuma meta selecionada!")
+        return
+    }
+
+    metas.forEach((m) => {
+        m.checked = false
+    })
+
+    respostas.forEach((resposta) => {
+       const meta = metas.find((m) => {
+        return m.value == resposta
+       })
+
+       meta.checked = true
+    })
+
+    console.log("Meta(s) marcadas como concluida(s)")
+}
+
 const start = async () => {
      while (true){
 
